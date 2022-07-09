3977b05609b80cd272646aa6c29a9a0210aa656d
deancn <dean@airdb.com>
2022-07-10 01:09:18 +0800

    update

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -20,24 +20,13 @@ export const sidebarEn: SidebarConfig = {
       ],
     },
   ],
-  '/advanced/': [
+  '/work/': [
     {
-      text: 'Advanced',
+      text: 'work',
       children: [
-        '/advanced/architecture.md',
-        '/advanced/plugin.md',
-        '/advanced/theme.md',
-      ],
-    },
-    {
-      text: 'Cookbook',
-      children: [
-        '/advanced/cookbook/README.md',
-        '/advanced/cookbook/usage-of-client-config.md',
-        '/advanced/cookbook/adding-extra-pages.md',
-        '/advanced/cookbook/making-a-theme-extendable.md',
-        '/advanced/cookbook/passing-data-to-client-code.md',
-        '/advanced/cookbook/markdown-and-vue-sfc.md',
+        '/work/tech.md',
+        '/work/presentation.md',
+        '/work/ray-dalio.md',
       ],
     },
   ],
---
+++ docs/work/tech.md
@@ -0,0 +1,5 @@
+# Tech
+
+Standardized codes
+
+without the leading slash
