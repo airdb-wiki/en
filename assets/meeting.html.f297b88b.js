ff44628f5853c51921ae632d54ba5cfb43991e30
deancn <deancn@users.noreply.github.com>
2022-07-16 17:48:12 +0000

    deploy: 43a8a42c933200f013f9be4acc63896cb061ecfd

---
+++ assets/meeting.html.f297b88b.js
@@ -0,0 +1,43 @@
+import{_ as e,o as n,c as s,a as i}from"./app.81d8c9c9.js";const a={},d=i(`<h1 id="meeting-phrases" tabindex="-1"><a class="header-anchor" href="#meeting-phrases" aria-hidden="true">#</a> Meeting phrases</h1><p>Say you want to have a meeting</p><div class="language-text ext-text line-numbers-mode"><pre class="shiki" style="background-color:#1E1E1E;"><code><span class="line"><span style="color:#D4D4D4;">As you know, we\u2019re experiencing...
+I\u2019d like to arrange a meeting in my office
+to discuss...
+Let\u2019s have a meeting about..
+Let\u2019s set up a meeting to...
+</span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Make a suggestion</p><div class="language-text ext-text line-numbers-mode"><pre class="shiki" style="background-color:#1E1E1E;"><code><span class="line"><span style="color:#D4D4D4;">Arrangements
+Are you available on\u2026?
+Can you do\u2026?
+Shall we say 9am?
+Do you have time on\u2026?
+Is it OK for us to meet on\u2026?
+Can we do...instead?
+How about\u2026?
+What about\u2026?
+What if we\u2026.?
+Is it possible to\u2026?
+Why don\u2019t we\u2026?
+I suggest we go...
+</span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Say yes</p><div class="language-text ext-text line-numbers-mode"><pre class="shiki" style="background-color:#1E1E1E;"><code><span class="line"><span style="color:#D4D4D4;">Arrangements
+Wednesday can work.
+I can make Wednesday.
+I can do Wednesday.
+Sounds fine.
+
+Sure.
+Sounds good.
+Good idea.
+</span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Say no</p><div class="language-text ext-text line-numbers-mode"><pre class="shiki" style="background-color:#1E1E1E;"><code><span class="line"><span style="color:#D4D4D4;">Arrangements
+I\u2019m afraid I can\u2019t make it.
+I\u2019m not sure I can\u2026.
+
+Actually,...
+Maybe, but\u2026.
+I\u2019m sorry, but\u2026.
+I\u2019d rather not.
+</span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Respond when someone says yes</p><div class="language-text ext-text line-numbers-mode"><pre class="shiki" style="background-color:#1E1E1E;"><code><span class="line"><span style="color:#D4D4D4;">Great!
+Totally agree
+</span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>Respond when someone says no</p><div class="language-text ext-text line-numbers-mode"><pre class="shiki" style="background-color:#1E1E1E;"><code><span class="line"><span style="color:#D4D4D4;">Oh right.
+No worries.
+Oh I see.
+Don\u2019t worry about it.
+OK no problem.
+</span></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,13),l=[d];function r(c,o){return n(),s("div",null,l)}var v=e(a,[["render",r],["__file","meeting.html.vue"]]);export{v as default};
