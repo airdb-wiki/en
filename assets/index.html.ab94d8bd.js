0baa17827096631451ca42ba71ac08ea7a715b23
deancn <deancn@users.noreply.github.com>
2022-08-12 03:15:46 +0000

    deploy: 191e74966419bb904e05da3a196867e88d098a7b

--- assets/index.html.ab94d8bd.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-14e6315a","path":"/life/","title":"Life","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1660187274000,"contributors":[{"name":"deancn","email":"dean@airdb.com","commits":2}]},"filePathRelative":"life/README.md"}');export{e as data};
